ca5c35942b9889a0407d8f11fd5ba438f3d191d4
sumathi3399 <sumathivarma3399@gmail.com>
2022-03-01 20:11:49 +0530

    primitives types

--- day1/sumthi.js
+++ day1/sumthi.js
@@ -2,3 +2,14 @@ console.log("Hi Sumathi");
 let msg;
 msg=2;
 console.log(msg);
+
+// primitive types
+//number,boolean,string,undefined
+let n =9.2;
+console.log(typeof(n));
+let m = "hi";
+console.log(typeof(m));
+let t = true;
+console.log(typeof(t));
+let a;
+console.log(typeof(a));
